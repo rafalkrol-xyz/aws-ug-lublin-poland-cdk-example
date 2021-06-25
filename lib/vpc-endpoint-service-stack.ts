be4f14fcd8c62d5d3aaa5801e785be9b5986fd41
Rafal Krol <ameotoko1+github@gmail.com>
2021-06-25 20:17:34 +0200

    Add a VPC service

--- lib/vpc-endpoint-service-stack.ts
+++ lib/vpc-endpoint-service-stack.ts
@@ -2,9 +2,11 @@ import * as cdk from '@aws-cdk/core'
 import * as ec2 from '@aws-cdk/aws-ec2'
 import * as ecs from '@aws-cdk/aws-ecs'
 import * as ecsPatterns from '@aws-cdk/aws-ecs-patterns'
+import * as iam from '@aws-cdk/aws-iam'
 
 interface VpcEndpointServiceStackProps extends cdk.StackProps {
   providerVpc: ec2.Vpc,
+  consumerVpc: ec2.Vpc,
 }
 
 export class VpcEndpointServiceStack extends cdk.Stack {
@@ -19,10 +21,15 @@ export class VpcEndpointServiceStack extends cdk.Stack {
 
     exampleFargateService.service.connections.allowFromAnyIpv4(ec2.Port.tcp(80))
 
-    new ec2.VpcEndpointService(this, 'VpcEndpointService', {
+    const vpcEndpointService = new ec2.VpcEndpointService(this, 'VpcEndpointService', {
       vpcEndpointServiceLoadBalancers: [exampleFargateService.loadBalancer],
+      allowedPrincipals: [new iam.ArnPrincipal(`arn:aws:iam::${process.env.CDK_DEFAULT_ACCOUNT}:root`)],
+      acceptanceRequired: false,
     })
 
-    // new ec2.InterfaceVpcEndpointService('test',)
+    new ec2.InterfaceVpcEndpoint(this, 'InterfaceVpcEndpoint', {
+      vpc: props.consumerVpc,
+      service: new ec2.InterfaceVpcEndpointService(vpcEndpointService.vpcEndpointServiceName),
+    })
   }
 }
